e9b2bccc807e14aa7d6c7ce9854bb4ac20f704d1
fabiogaspar11 <82576971+fabiogaspar11@users.noreply.github.com>
2021-10-25 20:54:01 +0100

    My initial commit
    Initial configuration of the project (JAVA EE)

---
+++ PCR/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
@@ -0,0 +1,14 @@
+package pt.ipleiria.estg.dei.ei.dae.prc.ejbs;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+@Startup
+@Singleton
+public class ConfigBean {
+    @PostConstruct
+    public void populateDB() {
+        System.out.println("Hello Java EE!");
+    }
+}
+
