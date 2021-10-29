c254d5814282198a9f5edc536c69025f683f0c76
fabiogaspar11 <82576971+fabiogaspar11@users.noreply.github.com>
2021-10-29 22:55:05 +0100

    Working
    There is a problem with healthcareprofessionals or biomedicdatatypes

--- PRC-Client/.nuxt/router.js
+++ PRC-Client/.nuxt/router.js
@@ -4,17 +4,10 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-const _373b5e41 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
-=======
-=======
->>>>>>> Stashed changes
 const _5753310f = () => interopDefault(import('..\\pages\\administrators.vue' /* webpackChunkName: "pages/administrators" */))
 const _e6d1b6d2 = () => interopDefault(import('..\\pages\\patients.vue' /* webpackChunkName: "pages/patients" */))
 const _61bacd40 = () => interopDefault(import('..\\pages\\prescriptions.vue' /* webpackChunkName: "pages/prescriptions" */))
 const _a43c85ce = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
->>>>>>> Stashed changes
 
 const emptyFn = () => {}
 
@@ -41,7 +34,7 @@ export const routerOptions = {
     name: "prescriptions"
   }, {
     path: "/",
-    component: _373b5e41,
+    component: _a43c85ce,
     name: "index"
   }],
 
