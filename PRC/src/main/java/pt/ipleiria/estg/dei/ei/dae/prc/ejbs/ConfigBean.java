b19bfdd564e481e3c771e3aebf3540cfbf891d7a
fabiogaspar11 <82576971+fabiogaspar11@users.noreply.github.com>
2021-10-29 23:11:49 +0100

    config bean test

--- PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
+++ PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
@@ -29,6 +29,7 @@ public class ConfigBean {
         patientBean.create("fabiogaspar11", "Fábio Gaspar", "2191264@my.ipleiria.pt", "12345", "04/12/2001", "916364061", 123456789, 60, (float) 1.68);
         //biomedicDataTypeBean.create(1, "Febre", "ºC", 30, 45);
         administratorBean.create("carlAdmin", "Carl Carl", "ola@aaa.aaa", "carladmin123", "17/04/1960", "919191911");
+        administratorBean.create("carla", "Carl Carl", "ola@aaa.aaa", "carladmin123", "17/04/1960", "919191911");
 
         //healthcareProfessionalBean.create("medicaMaria", 987654321, "Maria Gomes", "maria_gomes@gmail.com", "1234", "22/09/2021", "924359214");
         //patientBean.create("fabiogaspar11","Fábio Gaspar","2191264@my.ipleiria.pt","12345","04/12/2001","916364061",123456789,60,(float)1.68, "medicaMaria");
