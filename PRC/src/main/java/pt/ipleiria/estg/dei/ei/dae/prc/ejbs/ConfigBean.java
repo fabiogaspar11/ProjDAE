fc92e6614c2dde36cab9a68712c4227924c87754
CcostaX <carlos.cost@live.com.pt>
2021-10-30 16:53:10 +0100

    configBean

--- PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
+++ PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
@@ -31,7 +31,6 @@ public class ConfigBean {
         patientBean.create("fabiogaspar11","Fábio Gaspar", "2191264@my.ipleiria.pt", "12345", "04/12/2001", "916364061", 123456789, 60, (float) 1.68);
         patientBean.create("carlamendes12","Carla Mendes","2191111@my.ipleiria.pt","12345","05/06/2001","916345345",123456789,60,(float)1.68);
          healthcareProfessionalBean.create("medicaMaria", 987654321, "Maria Gomes", "maria_gomes@gmail.com", "1234", "22/09/2021", "924359214", "medic");
-        patientBean.create("fabiogaspar11", "Fábio Gaspar", "2191264@my.ipleiria.pt", "12345", "04/12/2001", "916364061", 123456789, 60, (float) 1.68);
 
         administratorBean.create("carlAdmin", "Carl Carl", "ola@aaa.aaa", "carladmin123", "17/04/1960", "919191911");
         //administratorBean.create("carla", "Carl Carl", "ola@aaa.aaa", "carladmin123", "17/04/1960", "919191911");
@@ -39,7 +38,6 @@ public class ConfigBean {
 
         //healthcareProfessionalBean.create("medicaMaria", 987654321, "Maria Gomes", "maria_gomes@gmail.com", "1234", "22/09/2021", "924359214");
         //patientBean.create("fabiogaspar11","Fábio Gaspar","2191264@my.ipleiria.pt","12345","04/12/2001","916364061",123456789,60,(float)1.68, "medicaMaria");
-        patientBean.create("carlamendes12","Carla Mendes","2191111@my.ipleiria.pt","12345","05/06/2001","916345345",123456789,60,(float)1.68);
         //patientBean.create("carloscosta13","Carlos Costa","2198322@my.ipleiria.pt","12345","14/03/2001","916364432",123456789,60,(float)1.68, "medicaMaria");
 
 
