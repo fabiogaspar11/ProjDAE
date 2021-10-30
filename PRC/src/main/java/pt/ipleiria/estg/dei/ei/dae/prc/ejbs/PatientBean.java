3f6dc52876ee628af268e6259ae5feab0c137f1f
carla <2191170@my.ipleiria.pt>
2021-10-30 20:59:07 +0100

    Patient Services
    Patient Services (missing everything that interacts with Prescription)

--- PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/PatientBean.java
+++ PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/PatientBean.java
@@ -1,6 +1,10 @@
 package pt.ipleiria.estg.dei.ei.dae.prc.ejbs;
 
+import pt.ipleiria.estg.dei.ei.dae.prc.dtos.PatientDTO;
+import pt.ipleiria.estg.dei.ei.dae.prc.entities.Disease;
+import pt.ipleiria.estg.dei.ei.dae.prc.entities.HealthcareProfessional;
 import pt.ipleiria.estg.dei.ei.dae.prc.entities.Patient;
+import pt.ipleiria.estg.dei.ei.dae.prc.entities.Prescription;
 import pt.ipleiria.estg.dei.ei.dae.prc.exceptions.MyEntityExistsException;
 import pt.ipleiria.estg.dei.ei.dae.prc.exceptions.MyEntityNotFoundException;
 
@@ -34,6 +38,103 @@ public class PatientBean {
         return patient;
     }
 
-    //TODO update Patient - cant update healthNumber!!!
+    public void remove(Patient patient) throws MyEntityNotFoundException {
+        for (HealthcareProfessional p:
+                patient.getHealthcareProfessionals()) {
+            removeHealthcareprofessionalFromPatient(p.getUsername(), patient.getUsername());
+            patient=entityManager.find(Patient.class,patient.getUsername());
+        }
 
+        for (Disease d:
+                patient.getDiseases()) {
+            removeDiseaseFromPatient(patient.getUsername(),d.getCode());
+            patient=entityManager.find(Patient.class,patient.getUsername());
+        }
+
+        entityManager.remove(entityManager.merge(patient));
+    }
+
+    public boolean removeHealthcareprofessionalFromPatient(String usernamePatient, String usernameHealthcareprofessional) throws MyEntityNotFoundException {
+        Patient patient = findPatient(usernamePatient);
+        HealthcareProfessional healthcareProfessional = entityManager.find(HealthcareProfessional.class, usernameHealthcareprofessional);
+        if (healthcareProfessional == null){
+            throw new MyEntityNotFoundException("There is no Healthcare professional with the username: \'"+usernamePatient+"\'");
+        }
+        patient.removeHealthcareProfessional(healthcareProfessional);
+        healthcareProfessional.removePatient(patient);
+        entityManager.merge(patient);
+        entityManager.merge(healthcareProfessional);
+        return true;
+    }
+
+    public boolean removeDiseaseFromPatient(String usernamePatient, int code) throws MyEntityNotFoundException {
+        Patient patient = findPatient(usernamePatient);
+        Disease disease = entityManager.find(Disease.class, code);
+        if (disease == null){
+            throw new MyEntityNotFoundException("There is no Disease with the code: \'"+code+"\'");
+        }
+        patient.removeDisease(disease);
+        disease.removePatient(patient);
+        entityManager.merge(patient);
+        entityManager.merge(disease);
+        return true;
+    }
+
+    public void update(Patient patient, PatientDTO patientDTO) {
+        if(patientDTO.getName() != null && !patient.getName().equals(patientDTO.getName())){
+            patient.setName(patientDTO.getName());
+        }
+        if(patientDTO.getPassword() != null && !patient.getPassword().equals(patientDTO.getPassword())){
+            patient.setPassword(patientDTO.getPassword());
+        }
+        if(patientDTO.getEmail() != null && !patient.getEmail().equals(patientDTO.getEmail())){
+            patient.setEmail(patientDTO.getEmail());
+        }
+
+        if(patientDTO.getBirthDate() != null && !patient.getBirthDate().equals(patientDTO.getBirthDate())){
+            patient.setBirthDate(patientDTO.getBirthDate());
+        }
+
+        if(patientDTO.getContact() != null && !patient.getContact().equals(patientDTO.getContact())){
+            patient.setContact(patientDTO.getContact());
+        }
+
+        if(patientDTO.getHeight() != 0 && patient.getHeight()  != patientDTO.getHeight()){
+            patient.setHeight(patientDTO.getHeight());
+        }
+
+        if(patientDTO.getWeight() != 0 && patient.getWeight()  != patientDTO.getWeight()){
+            patient.setWeight(patientDTO.getWeight());
+        }
+        entityManager.merge(patient);
+    }
+
+
+    public boolean addPrescription(String username, String code) throws MyEntityNotFoundException {
+        Patient patient = findPatient(username);
+        Prescription prescription = entityManager.find(Prescription.class, code);
+        if (prescription == null){
+            throw new MyEntityNotFoundException("There is no Prescription with the code: \'"+code+"\'");
+        }
+        patient.addPrescription(prescription);
+        //TODO
+        //prescription.setPatient(patient);
+        entityManager.merge(patient);
+        entityManager.merge(prescription);
+        return true;
+    }
+
+    public boolean removePrescription(String username, String code) throws MyEntityNotFoundException {
+        Patient patient = findPatient(username);
+        Prescription prescription = entityManager.find(Prescription.class, code);
+        if (prescription == null){
+            throw new MyEntityNotFoundException("There is no Prescription with the code: \'"+code+"\'");
+        }
+        patient.removePrescription(prescription);
+        //TODO
+        //prescription.setPatient(null);
+        entityManager.merge(patient);
+        entityManager.merge(prescription);
+        return true;
+    }
 }
