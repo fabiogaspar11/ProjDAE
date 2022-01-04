58723c507f76c63697810e10b88ffa7b8221e7b1
fabiogaspar11 <82576971+fabiogaspar11@users.noreply.github.com>
2022-01-04 19:11:32 +0000

    Update on confif bean data

--- PRC-Client/.nuxt/router.js
+++ PRC-Client/.nuxt/router.js
@@ -4,31 +4,31 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _a42c148a = () => interopDefault(import('..\\pages\\administrators\\index.vue' /* webpackChunkName: "pages/administrators/index" */))
-const _76b1d862 = () => interopDefault(import('..\\pages\\biomedicDataTypes\\index.vue' /* webpackChunkName: "pages/biomedicDataTypes/index" */))
-const _518fdb63 = () => interopDefault(import('..\\pages\\Dashboard.vue' /* webpackChunkName: "pages/Dashboard" */))
-const _33ba5c89 = () => interopDefault(import('..\\pages\\DashboardAdministrators.vue' /* webpackChunkName: "pages/DashboardAdministrators" */))
-const _6c70e68c = () => interopDefault(import('..\\pages\\DashboardHealthcareProfessionals.vue' /* webpackChunkName: "pages/DashboardHealthcareProfessionals" */))
-const _d3d0f9ac = () => interopDefault(import('..\\pages\\diseases\\index.vue' /* webpackChunkName: "pages/diseases/index" */))
-const _0793d0f4 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/index" */))
-const _3787119a = () => interopDefault(import('..\\pages\\patients\\index.vue' /* webpackChunkName: "pages/patients/index" */))
-const _1d0fdc60 = () => interopDefault(import('..\\pages\\auth\\Login.vue' /* webpackChunkName: "pages/auth/Login" */))
-const _5ed7e5c4 = () => interopDefault(import('..\\pages\\administrators\\_username.vue' /* webpackChunkName: "pages/administrators/_username" */))
-const _787c895c = () => interopDefault(import('..\\pages\\biomedicDataTypes\\_code.vue' /* webpackChunkName: "pages/biomedicDataTypes/_code" */))
-const _beb3fe6a = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/index" */))
-const _3e1d65a8 = () => interopDefault(import('..\\pages\\patients\\_username\\index.vue' /* webpackChunkName: "pages/patients/_username/index" */))
-const _79b1b858 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\biomedicMeasures\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/biomedicMeasures/index" */))
-const _0d8cf53d = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\patients\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/patients/index" */))
-const _fb27dda0 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\prescriptions\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/prescriptions/index" */))
-const _405de30e = () => interopDefault(import('..\\pages\\patients\\_username\\biomedicMeasures\\index.vue' /* webpackChunkName: "pages/patients/_username/biomedicMeasures/index" */))
-const _3d02b8e6 = () => interopDefault(import('..\\pages\\patients\\_username\\diseases.vue' /* webpackChunkName: "pages/patients/_username/diseases" */))
-const _682ecbce = () => interopDefault(import('..\\pages\\patients\\_username\\healthcareProfessionals.vue' /* webpackChunkName: "pages/patients/_username/healthcareProfessionals" */))
-const _02b66673 = () => interopDefault(import('..\\pages\\patients\\_username\\prescriptions.vue' /* webpackChunkName: "pages/patients/_username/prescriptions" */))
-const _7b7c6952 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\biomedicMeasures\\_code.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/biomedicMeasures/_code" */))
-const _2f7c8e16 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\patients\\_usernamePatient.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/patients/_usernamePatient" */))
-const _f7927bac = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\prescriptions\\_code.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/prescriptions/_code" */))
-const _3cc8811a = () => interopDefault(import('..\\pages\\patients\\_username\\biomedicMeasures\\_code.vue' /* webpackChunkName: "pages/patients/_username/biomedicMeasures/_code" */))
-const _373b5e41 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _f130b6da = () => interopDefault(import('..\\pages\\administrators\\index.vue' /* webpackChunkName: "pages/administrators/index" */))
+const _1f8f9a8a = () => interopDefault(import('..\\pages\\biomedicDataTypes\\index.vue' /* webpackChunkName: "pages/biomedicDataTypes/index" */))
+const _3dbb0e3b = () => interopDefault(import('..\\pages\\Dashboard.vue' /* webpackChunkName: "pages/Dashboard" */))
+const _b6f0953e = () => interopDefault(import('..\\pages\\DashboardAdministrators.vue' /* webpackChunkName: "pages/DashboardAdministrators" */))
+const _21c3fe98 = () => interopDefault(import('..\\pages\\DashboardHealthcareProfessionals.vue' /* webpackChunkName: "pages/DashboardHealthcareProfessionals" */))
+const _0fd73402 = () => interopDefault(import('..\\pages\\diseases\\index.vue' /* webpackChunkName: "pages/diseases/index" */))
+const _d8dd55c8 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/index" */))
+const _5dfc280b = () => interopDefault(import('..\\pages\\patients\\index.vue' /* webpackChunkName: "pages/patients/index" */))
+const _7fa1eea8 = () => interopDefault(import('..\\pages\\auth\\Login.vue' /* webpackChunkName: "pages/auth/Login" */))
+const _436e8ff6 = () => interopDefault(import('..\\pages\\administrators\\_username.vue' /* webpackChunkName: "pages/administrators/_username" */))
+const _215a4b84 = () => interopDefault(import('..\\pages\\biomedicDataTypes\\_code.vue' /* webpackChunkName: "pages/biomedicDataTypes/_code" */))
+const _718fd3a3 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/index" */))
+const _6d31b558 = () => interopDefault(import('..\\pages\\patients\\_username\\index.vue' /* webpackChunkName: "pages/patients/_username/index" */))
+const _e364f5a0 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\biomedicMeasures\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/biomedicMeasures/index" */))
+const _6a61cbd6 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\patients\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/patients/index" */))
+const _c5119950 = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\prescriptions\\index.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/prescriptions/index" */))
+const _5a202ca1 = () => interopDefault(import('..\\pages\\patients\\_username\\biomedicMeasures\\index.vue' /* webpackChunkName: "pages/patients/_username/biomedicMeasures/index" */))
+const _e9a80b36 = () => interopDefault(import('..\\pages\\patients\\_username\\diseases.vue' /* webpackChunkName: "pages/patients/_username/diseases" */))
+const _4637b841 = () => interopDefault(import('..\\pages\\patients\\_username\\healthcareProfessionals.vue' /* webpackChunkName: "pages/patients/_username/healthcareProfessionals" */))
+const _e2f4d2ca = () => interopDefault(import('..\\pages\\patients\\_username\\prescriptions.vue' /* webpackChunkName: "pages/patients/_username/prescriptions" */))
+const _dfcf93ac = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\biomedicMeasures\\_code.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/biomedicMeasures/_code" */))
+const _26e49d1d = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\patients\\_usernamePatient.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/patients/_usernamePatient" */))
+const _c17c375c = () => interopDefault(import('..\\pages\\healthCareProfessionals\\_username\\prescriptions\\_code.vue' /* webpackChunkName: "pages/healthCareProfessionals/_username/prescriptions/_code" */))
+const _5beadd9b = () => interopDefault(import('..\\pages\\patients\\_username\\biomedicMeasures\\_code.vue' /* webpackChunkName: "pages/patients/_username/biomedicMeasures/_code" */))
+const _a43c85ce = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -43,103 +43,103 @@ export const routerOptions = {
 
   routes: [{
     path: "/administrators",
-    component: _a42c148a,
+    component: _f130b6da,
     name: "administrators"
   }, {
     path: "/biomedicDataTypes",
-    component: _76b1d862,
+    component: _1f8f9a8a,
     name: "biomedicDataTypes"
   }, {
     path: "/Dashboard",
-    component: _518fdb63,
+    component: _3dbb0e3b,
     name: "Dashboard"
   }, {
     path: "/DashboardAdministrators",
-    component: _33ba5c89,
+    component: _b6f0953e,
     name: "DashboardAdministrators"
   }, {
     path: "/DashboardHealthcareProfessionals",
-    component: _6c70e68c,
+    component: _21c3fe98,
     name: "DashboardHealthcareProfessionals"
   }, {
     path: "/diseases",
-    component: _d3d0f9ac,
+    component: _0fd73402,
     name: "diseases"
   }, {
     path: "/healthCareProfessionals",
-    component: _0793d0f4,
+    component: _d8dd55c8,
     name: "healthCareProfessionals"
   }, {
     path: "/patients",
-    component: _3787119a,
+    component: _5dfc280b,
     name: "patients"
   }, {
     path: "/auth/Login",
-    component: _1d0fdc60,
+    component: _7fa1eea8,
     name: "auth-Login"
   }, {
     path: "/administrators/:username",
-    component: _5ed7e5c4,
+    component: _436e8ff6,
     name: "administrators-username"
   }, {
     path: "/biomedicDataTypes/:code",
-    component: _787c895c,
+    component: _215a4b84,
     name: "biomedicDataTypes-code"
   }, {
     path: "/healthCareProfessionals/:username",
-    component: _beb3fe6a,
+    component: _718fd3a3,
     name: "healthCareProfessionals-username"
   }, {
     path: "/patients/:username",
-    component: _3e1d65a8,
+    component: _6d31b558,
     name: "patients-username"
   }, {
     path: "/healthCareProfessionals/:username/biomedicMeasures",
-    component: _79b1b858,
+    component: _e364f5a0,
     name: "healthCareProfessionals-username-biomedicMeasures"
   }, {
     path: "/healthCareProfessionals/:username/patients",
-    component: _0d8cf53d,
+    component: _6a61cbd6,
     name: "healthCareProfessionals-username-patients"
   }, {
     path: "/healthCareProfessionals/:username/prescriptions",
-    component: _fb27dda0,
+    component: _c5119950,
     name: "healthCareProfessionals-username-prescriptions"
   }, {
     path: "/patients/:username/biomedicMeasures",
-    component: _405de30e,
+    component: _5a202ca1,
     name: "patients-username-biomedicMeasures"
   }, {
     path: "/patients/:username/diseases",
-    component: _3d02b8e6,
+    component: _e9a80b36,
     name: "patients-username-diseases"
   }, {
     path: "/patients/:username/healthcareProfessionals",
-    component: _682ecbce,
+    component: _4637b841,
     name: "patients-username-healthcareProfessionals"
   }, {
     path: "/patients/:username/prescriptions",
-    component: _02b66673,
+    component: _e2f4d2ca,
     name: "patients-username-prescriptions"
   }, {
     path: "/healthCareProfessionals/:username/biomedicMeasures/:code",
-    component: _7b7c6952,
+    component: _dfcf93ac,
     name: "healthCareProfessionals-username-biomedicMeasures-code"
   }, {
     path: "/healthCareProfessionals/:username/patients/:usernamePatient",
-    component: _2f7c8e16,
+    component: _26e49d1d,
     name: "healthCareProfessionals-username-patients-usernamePatient"
   }, {
     path: "/healthCareProfessionals/:username/prescriptions/:code",
-    component: _f7927bac,
+    component: _c17c375c,
     name: "healthCareProfessionals-username-prescriptions-code"
   }, {
     path: "/patients/:username/biomedicMeasures/:code",
-    component: _3cc8811a,
+    component: _5beadd9b,
     name: "patients-username-biomedicMeasures-code"
   }, {
     path: "/",
-    component: _373b5e41,
+    component: _a43c85ce,
     name: "index"
   }],
 
--- PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
+++ PRC/src/main/java/pt/ipleiria/estg/dei/ei/dae/prc/ejbs/ConfigBean.java
@@ -41,31 +41,33 @@ public class ConfigBean {
         healthcareProfessionalBean.create(900000001, "Maria Gomes", "mariagomes@mail.com", "12345", "22/09/1966", "924315414");
         healthcareProfessionalBean.create(900000002, "Ricardo Horta", "ricardoHorta@mail.com", "12345", "21/06/1987", "909000712");
         healthcareProfessionalBean.create(900000003, "Alicia Malhoa", "aliciaMalhoa@mail.com", "12345", "11/03/1998", "909212312");
-        healthcareProfessionalBean.create(900000004, "Anibal André", "anibalandre@mail.com", "12345", "12/03/1983", "901345621");
+        healthcareProfessionalBean.create(900000004, "Anibal Andre", "anibalandre@mail.com", "12345", "12/03/1983", "901345621");
         healthcareProfessionalBean.create(900000005, "Vasco Franco", "vascofranco@mail.com", "12345", "15/12/1982", "900000763");
         healthcareProfessionalBean.create(900000006, "Marco Santos", "vascofranco@mail.com", "12345", "01/09/1990", "900112763");
         String usernameH1 = healthcareProfessionalBean.create(900000007, "Clara Sousa", "clara@mail.com", "12345", "22/10/2000", "924359214");
 
-        String usernameP1 = patientBean.create("Fábio Gaspar", "2191264@my.ipleiria.pt", "04/12/2001", "916364061", 900000011,"Masculino");                     //20 Anos
-        String usernameP2 = patientBean.create("Carla Mendes","2191111@my.ipleiria.pt","05/06/2021","916345345",900000012,"Feminino");                          //6 Meses
-        String usernameP3 = patientBean.create("Carlos Costa","2191131@my.ipleiria.pt","11/04/2015","912343274",900000013,"Masculino");                         //6 Anos
-        String usernameP4 = patientBean.create("Daniel Carreira","2191215@my.ipleiria.pt","12/08/2008","917766763",900000014,"Masculino");                      //13 Anos
-        String usernameP5 = patientBean.create("Rafael Pereira","2191266@my.ipleiria.pt","14/06/2021","987786541",900000015,"Masculino");                       //6 meses
-        String usernameP6 = patientBean.create("Bruna Leitão","2191268@my.ipleiria.pt","23/06/2012","900086541",900000016,"Feminino");                         //9 Anos
-        patientBean.create("Rodrigo Domingues","2198732@my.ipleiria.pt","13/06/1950","901236541",900000017,"Masculino");                                        //71 Anos
+        String usernameP1 = patientBean.create("Fabio Gaspar", "fabio@my.ipleiria.pt", "04/12/2001", "916364061", 900000011,"Masculino");                     //20 Anos
+        String usernameP2 = patientBean.create("Carla Mendes","carla@my.ipleiria.pt","05/06/2021","916345345",900000012,"Feminino");                          //6 Meses
+        String usernameP3 = patientBean.create("Carlos Costa","carlos@my.ipleiria.pt","11/04/2015","912343274",900000013,"Masculino");                         //6 Anos
+        String usernameP4 = patientBean.create("Daniel Carreira","daniel@my.ipleiria.pt","12/08/2008","917766763",900000014,"Masculino");                      //13 Anos
+        String usernameP5 = patientBean.create("Rafael Pereira","rafael@my.ipleiria.pt","14/06/2021","987786541",900000015,"Masculino");                       //6 meses
+        String usernameP6 = patientBean.create("Bruna Leitao","bruna@my.ipleiria.pt","23/06/2012","900086541",900000016,"Feminino");                         //9 Anos
+        patientBean.create("Rodrigo Domingues","rodrigo@my.ipleiria.pt","13/06/1950","901236541",900000017,"Masculino");                                        //71 Anos
 
 
         administratorBean.create("Ricardo Reis","ricardo.reis@clinic.com", "12345", "17/04/1960", "919191911", 978675432);
         administratorBean.create("Alicia Mendes","alicia.mendes@clinic.com", "12345", "02/10/1966", "918347563", 156789014);
-        administratorBean.create("José Sousa","josesousa@clinic.com", "12345", "01/01/1977", "91214785", 156789999);
+        administratorBean.create("José Sousa","josesousa@clinic.com", "12345", "01/01/1977", "912147854", 156789999);
 
 
-        biomedicDataTypeBean.create("Temperatura Corporal","ºC",35,37.5F,30,45,0,0);
+        biomedicDataTypeBean.create( "Temperatura Corporal","ºC",35,37.5F,30,48,0,0);
         biomedicDataTypeBean.create( "Obesidade", "Kilogramas", 60, 100,0,300,-5,-5);
         biomedicDataTypeBean.create( "Pressão arterial sistólica", "mmHg", 100, 130,0,210,2,2);
         biomedicDataTypeBean.create( "Pressão arterial diastólica", "mmHg", 50, 80,0,100,2,2);
         biomedicDataTypeBean.create( "Frequência Cardíaca", "Batimentos", 70, 73,60,80,1,1);
         biomedicDataTypeBean.create( "Frequência Respiratória", "mpm", 16, 20,12,26,1,2);
+
+
 
         for(int i = 1; i < 8; i++){
             String code = String.valueOf(i);
@@ -79,11 +81,26 @@ public class ConfigBean {
                 prescriptionBean.create("Insuficiência cardíaca com fração de ejeção preservada","","Yes","Enalapril 10 a 20 mg","15/09/2021","12/06/2022","P90000001" + codePatient,"H90000000" + code);
                 prescriptionBean.create("Acumulação de gordura abdominal","","Yes","Caminhadas 6 dias por semana. O paciente deve ainda tomar vitaminas B12 durante 10 dias","03/10/2021","10/12/2022","P90000001" + codePatient,"H90000000" + code);
                 prescriptionBean.create("Pressão arterial - Controlo","","Yes","O paciente deve continuar com as caminhadas 6 dias por semana. O paciente deve ainda tomar vitaminas B12 e C até a proxima consulta","28/12/2021","28/12/2022","P90000001" + codePatient,"H90000000" + code);
-                value = 30+j;
-                biomedicDataMeasureBean.create(value,"01/01/2022","11:"+value,"P90000001" + code,2, "P90000001" + code);
             }
-
         }
+
+
+        biomedicDataMeasureBean.create(37,"01/01/2022","11:25","P900000011",1, "H900000001");
+        biomedicDataMeasureBean.create(130,"02/01/2022","11:27","P900000011",2, "P900000011");
+        biomedicDataMeasureBean.create(20,"02/01/2022","16:00","P900000011",5, "P900000011");
+
+        biomedicDataMeasureBean.create(115,"02/01/2022","16:16","P900000012",3, "H900000002");
+        biomedicDataMeasureBean.create(90,"29/12/2021","15:51","P900000012",4, "H900000005");
+        biomedicDataMeasureBean.create(42,"01/01/2022","05:06","P900000012",1, "H900000001");
+
+
+        biomedicDataMeasureBean.create(72,"02/01/2022","16:16","P900000013",5, "H900000003");
+        biomedicDataMeasureBean.create(17,"29/12/2021","15:51","P900000013",6, "H900000006");
+        biomedicDataMeasureBean.create(39,"01/01/2022","05:06","P900000013",1, "H900000001");
+
+        biomedicDataMeasureBean.create(36.5,"01/01/2022","05:23","P900000014",1, "H900000003");
+        biomedicDataMeasureBean.create(37.2,"01/01/2022","21:23","P900000014",1, "H900000004");
+
 
         long code1 = diseaseBean.create("Hipertensão");
         long code2 = diseaseBean.create("Infarto agudo do miocárdio");
